9e26596edcfcc4b7f48370ceecc5fde5754e4b80
Oshi41 <sheglovaoe@gmail.com>
2018-04-04 00:59:36 +0300

    Размер документа - A4

--- Districts/Printing/CustomDocumentPaginator.cs
+++ Districts/Printing/CustomDocumentPaginator.cs
@@ -18,6 +18,8 @@ namespace Districts.Printing
     /// </summary>
     internal class CustomDocumentPaginator : DocumentPaginator
     {
+        private static readonly bool USEA4 = true;
+
         private readonly List<Card> _cards;
         private readonly PrintCapabilities _printCapabilities;
         private Size _pageSize;
@@ -33,7 +35,7 @@ namespace Districts.Printing
             PageCount = (int)Math.Ceiling((decimal)(_cards.Count / 2));
             _printCapabilities = dlg.PrintQueue.GetPrintCapabilities(dlg.PrintTicket);
 
-            SetSize(_printCapabilities);
+            SetA4Size();
         }
 
 
@@ -48,26 +50,84 @@ namespace Districts.Printing
 
         public override IDocumentPaginatorSource Source => null;
 
-        private void SetSize(PrintCapabilities capabilities)
+        #region Size work
+
+        //8.27 x 11.69
+
+        private void SetA4Size()
         {
             var settings = new PrinterSettings();
+            var a4 = settings.PaperSizes
+                .OfType<PaperSize>()
+                .FirstOrDefault(x => x.Kind == PaperKind.A4);
+            _pageSize = new Size(a4.Width, a4.Height);
+        }
 
+        [Obsolete]
+        private void SetPrinterSize()
+        {
+            var settings = new PrinterSettings();
             // устанавливаю размер по принтеру
-            if (capabilities?.OrientedPageMediaWidth != null
-                && capabilities.OrientedPageMediaHeight.HasValue)
+            if (_printCapabilities?.OrientedPageMediaWidth.HasValue == true
+                && _printCapabilities.OrientedPageMediaHeight.HasValue)
             {
-                _pageSize = new Size((double)capabilities.OrientedPageMediaWidth,
-                                (double)capabilities.OrientedPageMediaHeight);
+                _pageSize = new Size((double)_printCapabilities.OrientedPageMediaWidth,
+                    (double)_printCapabilities.OrientedPageMediaHeight);
             }
-            // либо по а4
             else
             {
-                var a4 = settings.PaperSizes.OfType<PaperSize>().FirstOrDefault(x => x.Kind == PaperKind.A4);
-                _pageSize = new Size(a4.Width, a4.Height);
-
+                SetA4Size();
             }
         }
 
+        private void SetPrinterSize(PrintDialog dlg)
+        {
+            _pageSize = new Size(dlg.PrintableAreaWidth, dlg.PrintableAreaHeight);
+        }
+
+        private void SetDpiBasedA4Size(PrintDialog dlg)
+        {
+            // a4 inch size
+            var size = new Size(8.27, 11.69);
+            //get max DPI
+            var maxResolution = _printCapabilities.PageResolutionCapability
+                .OfType<PrinterResolution>()
+                .OrderByDescending(r => r.X * r.Y)
+                .FirstOrDefault();
+
+            if (maxResolution != null
+                && maxResolution.Y > 0
+                && maxResolution.X > 0)
+            {
+                size.Height *= maxResolution.Y;
+                size.Width *= maxResolution.X;
+            }
+            else
+            {
+                SetPrinterSize(dlg);
+            }
+        }
+
+        //private void SetSize(PrintCapabilities capabilities)
+        //{
+        //    var settings = new PrinterSettings();
+
+        //    // устанавливаю размер по принтеру
+        //    if (capabilities?.OrientedPageMediaWidth.HasValue == true
+        //        && capabilities.OrientedPageMediaHeight.HasValue)
+        //    {
+        //        _pageSize = new Size((double)capabilities.OrientedPageMediaWidth,
+        //                        (double)capabilities.OrientedPageMediaHeight);
+        //    }
+        //    // либо по а4
+        //    else
+        //    {
+        //        var a4 = settings.PaperSizes.OfType<PaperSize>().FirstOrDefault(x => x.Kind == PaperKind.A4);
+        //        _pageSize = new Size(a4.Width, a4.Height);
+
+        //    }
+        //}
+        #endregion
 
         public override DocumentPage GetPage(int pageNumber)
         {
