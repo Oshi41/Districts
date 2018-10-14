56a84d52cc19e5f6aad8601acdace2e1d6916277
Oshi41 <sheglovaoe@gmail.com>
2018-10-14 22:29:27 +0300

    Добавил пропущенный файл

---
+++ Districts/Backup/BackupManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Districts.Settings;
+using Ionic.Zip;
+using Microsoft.Win32;
+
+namespace Districts.Backup
+{
+    public class BackupManager
+    {
+        public void MakeBackup()
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "Aрхив 7-Zip (*.zip)|";
+
+            if (dlg.ShowDialog() != true)
+                return;
+
+            using (var zip = new ZipFile())
+            {
+                var settings = ApplicationSettings.ReadOrCreate();
+
+                zip.AddDirectory(settings.BuildingPath, Path.GetDirectoryName(settings.BuildingPath));
+                zip.AddDirectory(settings.CardsPath, Path.GetDirectoryName(settings.CardsPath));
+                zip.AddDirectory(settings.CodesPath, Path.GetDirectoryName(settings.CodesPath));
+                zip.AddDirectory(settings.RestrictionsPath, Path.GetDirectoryName(settings.RestrictionsPath));
+                zip.AddFile(settings.StreetsPath, Path.GetFileName(settings.StreetsPath));
+
+                zip.Comment = "Был создан в " + DateTime.Now.ToShortTimeString();
+                zip.Save(dlg.FileName);
+            }
+        }
+    }
+}
